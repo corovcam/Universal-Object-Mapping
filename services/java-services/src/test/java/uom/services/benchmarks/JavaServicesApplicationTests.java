f5d6eb0ced620a35786f7beb759355df22324f02
Martin Čorovčák <martino.coro@gmail.com>
2026-03-09 16:57:21 +0100

    Add java-services

---
+++ services/java-services/src/test/java/uom/services/benchmarks/JavaServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package uom.services.benchmarks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
