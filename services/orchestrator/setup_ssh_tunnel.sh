ed627a364bd6fff089da7475d1c39dce917b1927
Martin Čorovčák <martino.coro@gmail.com>
2026-03-11 17:34:19 +0100

    feat: Introduce .NET service and configure orchestrator with SSH tunneling using Docker Compose includes.

--- services/orchestrator/setup_ssh_tunnel.sh
+++ services/orchestrator/setup_ssh_tunnel.sh
@@ -5,24 +5,15 @@
 cp /root/.ssh/xrg13_key /tmp/xrg13_key
 chmod 600 /tmp/xrg13_key
 
-# Run ssh tunnel in a loop to reconnect on failure (since autossh is not available)
-(
-while true; do
-  echo "Establishing SSH tunnel..."
-  ssh -N \
-  -o "ExitOnForwardFailure=yes" \
-  -o "ServerAliveInterval=10" \
-  -o "ServerAliveCountMax=3" \
-  -i /tmp/xrg13_key \
-  -L "11434:localhost:11434 $XRG13_SSH_USERNAME@xrg13.ms.mff.cuni.cz" \
-  -p 1302 \
-  -o StrictHostKeyChecking=no \
-  -o UserKnownHostsFile=/dev/null
-  
-  echo "SSH tunnel disconnected. Reconnecting in 5 seconds..."
-  sleep 5
-done
-) &
+autossh -f -M 0 -gN \
+    -o "ExitOnForwardFailure=yes" \
+    -o "ServerAliveInterval=10" \
+    -o "ServerAliveCountMax=3" \
+    -i /tmp/xrg13_key \
+    -L 11434:localhost:11434 $XRG13_SSH_USERNAME@xrg13.ms.mff.cuni.cz \
+    -p 1302 \
+    -o StrictHostKeyChecking=no \
+    -o UserKnownHostsFile=/dev/null
 
 timeout=30
 while ! nc -z localhost 11434; do
