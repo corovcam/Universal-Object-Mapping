c90d25cb35abb4576991d5845028556ca2f05197
Martin Čorovčák <martino.coro@gmail.com>
2026-03-23 13:57:15 +0100

    feat: Externalize service configurations and credentials into environment variables and add a MongoDB initialization script.

---
+++ services/etl/mongodb/init-mongodb.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mongo -- "$MONGO_INITDB_DATABASE" <<EOF
+    var rootUser = '$MONGO_INITDB_ROOT_USERNAME';
+    var rootPassword = '$MONGO_INITDB_ROOT_PASSWORD';
+    var user = '$MONGO_INITDB_USERNAME';
+    var passwd = '$MONGO_INITDB_USERNAME';
+    var admin = db.getSiblingDB('admin');
+    admin.auth(rootUser, rootPassword);
+    db.createUser({user: user, pwd: passwd, roles: [{role:"read",db:"$MONGO_INITDB_DATABASE"}]});
+EOF
