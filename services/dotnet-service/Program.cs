8b5baf33604aeb7fa389a29c478b7433373503f3
Martin Čorovčák <martino.coro@gmail.com>
2026-04-20 19:41:41 +0200

    Refactor code structure for improved readability and maintainability

--- services/dotnet-service/Program.cs
+++ services/dotnet-service/Program.cs
@@ -1,9 +1,17 @@
+using System.Text.Json.Serialization;
+using DotNetService.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-builder.Services.AddControllers();
+builder.Services
+    .AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
 builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
